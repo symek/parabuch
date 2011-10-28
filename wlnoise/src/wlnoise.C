f819a475a83324e29aeeb7cd664f5273741a70eb
szymon.kapeniak@gmail.com <szymon.kapeniak@gmail.com@043e67e9-1402-f124-75ab-99bc2a501741>
2011-10-28 18:46:13 +0000

    Multiband Projected Noise added (after plain wavelet and projected) . Sample scene. Still workinprogress.

--- wlnoise/src/wlnoise.C
+++ wlnoise/src/wlnoise.C
@@ -1,10 +1,17 @@
+/* The main bulk of this code comes from "Wavelet Noise" 
+*  by Robert L. Cook and Tony DeRose,
+*  from Pixar Animation Studios: 
+*  http://graphics.pixar.com/library/WaveletNoise/paper.pdf
+*/
+
+#include <stdio.h>
 #include <VEX/VEX_VexOp.h>
 #include <UT/UT_Vector3.h>
 #include <UT/UT_DSOVersion.h>
 #include <UT/UT_MTwister.h>
 #define ARAD 16
-#define SIZE 64
-#define OLAP 2
+#define SIZE 128
+#define OLAP 8
 
 // Static tile array and its size:
 static float *noiseTileData = NULL; 
@@ -54,14 +61,14 @@ void GenerateNoiseTile( int n, int olap)
     UT_MersenneTwister *twister = new UT_MersenneTwister();
 
     /* Step 1. Fill the tile with random numbers in the range -1 to 1. */
-    cout << "Twister: ";
+    //cout << "Twister: ";
     for (i=0; i<n*n*n; i++) 
     {
         //noise[i] = gaussianNoise();
         noise[i]   = twister->frandom0()*2.0;
-        if (i < 10 ) cout << noise[i] << ",";
+       // if (i < 10 ) cout << noise[i] << ",";
     }
-    cout << "..." << endl;
+    //cout << "..." << endl;
         
     /* Steps 2 and 3. Downsample and upsample the tile */
     for (iy=0; iy<n; iy++) for (iz=0; iz<n; iz++) 
@@ -105,13 +112,81 @@ void GenerateNoiseTile( int n, int olap)
     free(temp1); 
     free(temp2);
     delete twister;
-    cout << "Noise tile: ";
-    for (int i = 0; i < 10; i++)
-        cout << noiseTileData[i] << ",";
-    cout << endl;
+    //cout << "Noise tile: ";
+   // for (int i = 0; i < 10; i++)
+   //     cout << noiseTileData[i] << ",";
+    //cout << endl;
 }
 
+float 
+WProjectedNoise(float *p, float *normal)
+{
+    /* 3D noise projected onto 2D */
+    int i, c[3], min[3], max[3], n=SIZE;
+    /* c = noise coeff location */
+    float support, result=0;
+    /* Bound the support of the basis functions for this projection direction */
+    for (i=0; i<3; i++) 
+    {
+        support = 3*abs(normal[i]) + 3*sqrt((1-normal[i]*normal[i])/2);
+        min[i] =  ceil( p[i] - (3*abs(normal[i]) + 3*sqrt((1-normal[i]*normal[i])/2)) );
+        max[i] = floor( p[i] + (3*abs(normal[i]) + 3*sqrt((1-normal[i]*normal[i])/2)) ); }
+        /* Loop over the noise coefficients within the bound. */
+        for(c[2]=min[2];c[2]<=max[2];c[2]++) 
+        {
+            for(c[1]=min[1];c[1]<=max[1];c[1]++) 
+            {
+                for(c[0]=min[0];c[0]<=max[0];c[0]++) 
+                {
+                    float t, t1, t2, t3, dot=0, weight=1;
+                    /* Dot the normal with the vector from c to p */
+                    for (i=0; i<3; i++) {dot+=normal[i]*(p[i]-c[i]);}
+                    /* Evaluate the basis function at c moved halfway to p along the normal. */
+                    for (i=0; i<3; i++) 
+                    {
+                        t = (c[i]+normal[i]*dot/2)-(p[i]-1.5); t1=t-1; t2=2-t; t3=3-t;
+                        weight*=(t<=0||t>=3)? 0 : (t<1) ? t*t/2 : (t<2)? 1-(t1*t1+t2*t2)/2 : t3*t3/2;
+                    }
+                /* Evaluate noise by weighting noise coefficients by basis function values. */
+                result += weight * noiseTileData[Mod(c[2],n)*n*n+Mod(c[1],n)*n+Mod(c[0],n)];
+                }
+           }
+      }
+    return result;
+}
 
+float
+WNoise(float *p)
+{
+    /* f, c = filter, noise coeff indices */
+    int i, f[3], c[3], mid[3], n = SIZE; 
+    float w[3][3], t, result = 0;
+    
+    /* Evaluate quadratic B-spline basis functions */
+    for (i=0; i<3; i++) 
+    {
+        mid[i] = ceil(p[i] - 0.5);
+        t      = mid[i]-(p[i] - 0.5);
+        w[i][0] = t*t/2; 
+        w[i][2] = (1 - t)*(1 - t)/2; 
+        w[i][1] = 1 - w[i][0] - w[i][2]; 
+     }
+        /* Evaluate noise by weighting noise coefficients by basis function values */
+        for(f[2] = -1; f[2] <= 1; f[2]++) 
+            for(f[1] = -1; f[1] <= 1; f[1]++) 
+                for(f[0]=-1; f[0] <= 1; f[0]++) 
+                {
+                    float weight = 1;
+                    for (i = 0; i<3; i++) 
+                    {
+                        c[i]    = Mod(mid[i] + f[i],n); 
+                        weight *= w[i][f[i] + 1];
+                    }
+                    result += weight * noiseTileData[c[2]*n*n+c[1]*n+c[0]]; 
+                }
+                
+    return result;
+}
 
 static void *
 my_init() 
@@ -134,13 +209,12 @@ my_cleanup(void *data)
     UT_ASSERT(tile == noiseTileData);
     refCounter--;
      cout << "refCounter-- " << refCounter << endl;
-    if (refCounter == 0)
+    if (refCounter <= 0)
     {
         cout << "Cleaning tile." << endl;
         delete tile;
         noiseTileData = NULL;
     }
-
 }
 
 static void 
@@ -148,51 +222,72 @@ wlnoise(int, void *argv[], void *data)
 {
 
     const UT_Vector3 *pos   = (const UT_Vector3 *) argv[1]; 
-    //const UT_Vector3 *scale = 1;//(const UT_Vector3 *) argv[2];
 	      float      *noise = (float*) argv[0];
-	float            *tile  = (float *) data;
+	      //float      *tile  = (float *) data;
 
-
-    /* f, c = filter, noise coeff indices */
-    int i, f[3], c[3], mid[3], n = SIZE; 
-    
-    float w[3][3], t, result = 0;
-    
-    UT_Vector3 *scale = new UT_Vector3(1.0, 1.0, 1.0);
     float p[3];
-    p[0] = pos->x()*scale->x(); p[1] = pos->y()*scale->y(); p[2] = pos->z()*scale->z();
+    p[0] = pos->x(); p[1] = pos->y(); p[2] = pos->z();
+    noise[0] = WNoise(p);
     
-   
-    
-    /* Evaluate quadratic B-spline basis functions */
-    for (i=0; i<3; i++) 
-    {
-        mid[i] = ceil(p[i] - 0.5);
-        t      = mid[i]-(p[i] - 0.5);
-        w[i][0] = t*t/2; 
-        w[i][2] = (1 - t)*(1 - t)/2; 
-        w[i][1] = 1 - w[i][0] - w[i][2]; 
-     }
-        /* Evaluate noise by weighting noise coefficients by basis function values */
-        for(f[2] = -1; f[2] <= 1; f[2]++) 
-            for(f[1] = -1; f[1] <= 1; f[1]++) 
-                for(f[0]=-1; f[0] <= 1; f[0]++) 
-                {
-                    float weight = 1;
-                    for (i = 0; i<3; i++) 
-                    {
-                        c[i]    = Mod(mid[i] + f[i],n); 
-                        weight *= w[i][f[i] + 1];
-                    }
-                    result += weight * tile[c[2]*n*n+c[1]*n+c[0]]; 
-                }
-                
-    noise[0] = result;
-
 }
 
 
+static void
+wlpnoise(int, void *argv[], void *data)
+{
+    const UT_Vector3 *pos     = (const UT_Vector3 *) argv[1]; 
+    const UT_Vector3 *nor     = (const UT_Vector3 *) argv[2];
+	      float      *noise   = (float*) argv[0];
+	     // float      *tile    = (float *) data;
+	
+	float p[3], normal[3];
+    p[0] = pos->x();      p[1] = pos->y();      p[2] = pos->z();
+    normal[0] = nor->x(); normal[1] = nor->y(); normal[2] = nor->z();
+    noise[0]  = WProjectedNoise(p, normal);
+}
 
+static void
+wlmnoise(int, void *argv[], void *data) 
+{
+    const UT_Vector3 *pos     = (const UT_Vector3 *) argv[1];
+    const float      *ss      = (const float *)      argv[2];  
+    const UT_Vector3 *nor     = (const UT_Vector3 *) argv[3];
+    const int        *fb      = (const int*)         argv[4];
+    const int        *nb      = (const int*)         argv[5];
+    const UT_Vector3 *ww      = (const UT_Vector3 *) argv[6]; // TODO: we need array here. 
+	      float      *noise   = (float*) argv[0];
+	      //float      *tile    = (float *) data;
+     
+    float p[3], normal[3]; float w[3];
+    p[0] = pos->x();      p[1] = pos->y();      p[2] = pos->z();
+    normal[0] = nor->x(); normal[1] = nor->y(); normal[2] = nor->z();
+    float s = *ss; int firstBand = *fb; int nbands = *nb;
+    w[0] = ww->x();      w[1] = ww->y();      w[2] = ww->z();
+    
+    //printf("%f, %i, %i, %f%f%f", s, firstBand, nbands, w[0], w[1], w[2]);
+     
+    //float p[3],float s,float *normal,int firstBand,int nbands,float *w
+    float q[3], result=0, variance=0; int i, b;
+
+    for (b=0; b<nbands && s+firstBand+b<0; b++) 
+    {
+        //cout << "Iam in!" << endl;
+        for (i=0; i<=2; i++) 
+            q[i]=2*p[i]*pow(2,firstBand+b);
+        if (!normal)
+            result +=  w[b] * WProjectedNoise(q,normal);
+        else
+            result += w[b] * WNoise(q);
+    }   
+    for (b=0; b<nbands; b++) 
+        variance+=w[b]*w[b];
+    /* Adjust the noise so it has a variance of 1. */
+    if (variance) 
+        result /= sqrt(variance * ((!normal) ? 0.296 : 0.210));
+        
+        
+    noise[0] = result;
+}
 
 
 
@@ -201,7 +296,21 @@ void
 newVEXOp(void *)
 {
 	new VEX_VexOp("wlnoise@&FV", wlnoise, 
-			VEX_ALL_CONTEXT, 
+			VEX_SURFACE_CONTEXT, /* FIXME: This is not thread save code, thus only mantra can handle it */
+			my_init,
+			my_cleanup, 
+			VEX_OPTIMIZE_2,
+			true);
+			
+    new VEX_VexOp("wlpnoise@&FVV", wlpnoise, 
+			VEX_SURFACE_CONTEXT, /* FIXME: This is not thread save code, thus only mantra can handle it */
+			my_init,
+			my_cleanup, 
+			VEX_OPTIMIZE_2,
+			true);
+			
+    new VEX_VexOp("wlmnoise@&FVFVIIV", wlmnoise, 
+			VEX_SURFACE_CONTEXT, /* FIXME: This is not thread save code, thus only mantra can handle it */
 			my_init,
 			my_cleanup, 
 			VEX_OPTIMIZE_2,
