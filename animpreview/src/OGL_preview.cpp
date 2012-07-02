8afc50df73dae9e3b38372e823ee7a776cfdeeb9
szymon.kapeniak@gmail.com <szymon.kapeniak@gmail.com@043e67e9-1402-f124-75ab-99bc2a501741>
2012-07-02 22:16:21 +0000

    [animpreview] Small tweak in Makefile (specif irrlicht home by env. variable). WIP: camera, meshes.

--- animpreview/src/OGL_preview.cpp
+++ animpreview/src/OGL_preview.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <math.h>
 
 // Almost own..., 
 // but I'm steeling pystring's functions:
@@ -26,9 +27,10 @@ using namespace gui;
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
-typedef std::vector<std::vector<float> > cameraParms;
 
 
+// Basic class to handle camera animation storage
+// TODO: Build in json support, extend, switch to std::vectors<> etc
 class JSONCamera
 {
 public:
@@ -79,9 +81,9 @@ create_mesh(std::string &mesh_file, ISceneManager *smgr, )
 */
 
 // JSON support:
-
 inline void copy_json_array(picojson::object::const_iterator &item, float *channel)
 {
+    // Copy picojson::array into float[] TODO: Replace to std::vector<float>
     int idx = 0;
     const picojson::array& a = (item->second).get<picojson::array>();    
     for (picojson::array::const_iterator i = a.begin(); i != a.end(); ++i)
@@ -91,34 +93,38 @@ inline void copy_json_array(picojson::object::const_iterator &item, float *chann
     }
 }
 
-bool parse_camera_file(std::string camera_file, Camera &camera)
+bool parse_camera_file(const char *filename, JSONCamera &camera, int verbose=0)
 {
-    picojson::value json;
-    std::ifstream file_json;
-    file_json.open(camera_file.data());
-    file_json >> json;
-    file_json.close();
-    //std::cout << json << std::endl;
+    // Read in Json object:
+    picojson::value json_object;
+    std::ifstream json_file;
+    json_file.open(filename);
+    json_file >> json_object;
+    json_file.close();
+    // Print content:
+    if (verbose)
+        std::cout << json_object << std::endl;
 
-    if (json.is<picojson::object>()) 
+    // Parse and load in arrays into JSONCamera:
+    if (json_object.is<picojson::object>()) 
     {   
-	    const picojson::object& o = json.get<picojson::object>();
+	    const picojson::object& o = json_object.get<picojson::object>();
 	    for (picojson::object::const_iterator i = o.begin(); i != o.end(); ++i)
         {
                  if (i->first == std::string("tx"))
                 copy_json_array(i, camera.tx);
             else if (i->first == std::string("ty"))
-                copy_json_array(i, camera[1]);
+                copy_json_array(i, camera.ty);
             else if (i->first == std::string("tz"))
-                copy_json_array(i, camera.ty]);
-            else if (i->first == std::string("rx"))
                 copy_json_array(i, camera.tz);
-            else if (i->first == std::string("ry"))
+            else if (i->first == std::string("rx"))
                 copy_json_array(i, camera.rx);
-            else if (i->first == std::string("rz"))
+            else if (i->first == std::string("ry"))
                 copy_json_array(i, camera.ry);
-            else if (i->first == std::string("focal"))
+            else if (i->first == std::string("rz"))
                 copy_json_array(i, camera.rz);
+            else if (i->first == std::string("focal"))
+                copy_json_array(i, camera.focal);
 	    }
         return true;
     }
@@ -136,7 +142,7 @@ int main(int argc, char *argv[])
     const std::string seperator(",");         
     std::vector<std::string> geos;
     std::vector<std::string> pc2s;
-    std::string       camera_file;
+    const char       *camera_file;
 
     // Parsing Command line argumentes:
     // OBJs:
@@ -154,24 +160,17 @@ int main(int argc, char *argv[])
         split(buffer, pc2s, seperator, 1000);
     }
     // Camera
-    tmp = get_argument(argv, argv+argc, "-c");
-    if (tmp)
+    camera_file = get_argument(argv, argv+argc, "-c");
+    if (!camera_file)
     {
-        camera_file.assign(tmp);
+        std::cout << "No camera specified." << std::endl;
     }
 
     // Read camera from json file:
     JSONCamera json_camera(200);
     if (!parse_camera_file(camera_file, json_camera))
         std::cout << "Can't read camera" << std::endl;
-    else
-        for (int i = 0; i < 20; i++)
-        {
-            std::cout << json_camera.tx[i] << ", ";
-            std::cout << json_camera.ty[i] << ", ";
-            std::cout << json_camera.tz[i] << ", ";
-        }
-    return 0;
+    
 
     // Create a device (OpenGL or Software in fall-back case):
     IrrlichtDevice *device = NULL;
@@ -200,19 +199,28 @@ int main(int argc, char *argv[])
     }
 
 
+    // Add camera:
+    ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya();
+    camera->setPosition(vector3df(json_camera.tx[0], json_camera.ty[0], json_camera.tz[0]));
+    camera->setRotation(vector3df(json_camera.rx[0], json_camera.ry[0], json_camera.rz[0]));
+    float fov = 2 * atan( (24.0/2.0) / 40 );
+    camera->setFOV(fov);
+
+    // Lights:     
+	smgr->addLightSceneNode(camera, core::vector3df(300,100,-50), video::SColorf(1.0f,1.0f,1.0f),300);
+	smgr->setAmbientLight(video::SColorf(0.3f,0.3f,0.3f));
+
+
     // Capition:
     //const char *title = sprintf("Animation preview. Maximum primitives: %i", max_prims);
     device->setWindowCaption(L"Animation preview.");
-
     // On screen massage:
     guienv->addStaticText(L"This is hardware render_preview test", rect<s32>(10,10,260,22), true);
-
-
     // Main rendering loop:
     while(device->run())
 	{
         // SColor(255,100,101,140)
-		driver->beginScene(true, true, SColor(11,11,11,11));
+		driver->beginScene(true, true, SColor(110,110,110,110));
 		smgr->drawAll();
 		guienv->drawAll();
 		driver->endScene();
