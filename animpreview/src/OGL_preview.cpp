1adb008ebde1efd7bcb772c92d8c82b018222595
szymon.kapeniak@gmail.com <szymon.kapeniak@gmail.com@043e67e9-1402-f124-75ab-99bc2a501741>
2012-07-03 16:05:42 +0000

    [animpreview] Got stucked with camera animation and mesh updates, code broken atm.

--- animpreview/src/OGL_preview.cpp
+++ animpreview/src/OGL_preview.cpp
@@ -68,6 +68,27 @@ enum
 	IDFlag_IsHighlightable = 1 << 1
 };
 
+//http://julien.boucaron.free.fr/wordpress/?p=269
+struct IrrlichtDelayFPS {
+  int maxFPS;
+  int lastFPS;
+  int yieldCounter;
+  IrrlichtDevice *device;
+  IrrlichtDelayFPS() { maxFPS = 50;  yieldCounter = 0;  device = 0; }
+  void delay() {
+    video::IVideoDriver* driver = device->getVideoDriver();
+    int lastFPS = driver->getFPS();
+    for( int i = 0 ; i < yieldCounter ; i++ ) {
+      device->yield();
+    }
+    if ( lastFPS >maxFPS ) { yieldCounter++ ; }
+    else {
+      if (yieldCounter > 0 ) { --yieldCounter; }
+      else { yieldCounter = 0 ; }
+    }
+  }
+};
+
 
 /*
 IAnimatedMesh* 
@@ -189,33 +210,87 @@ int main(int argc, char *argv[])
 
 
      // Create meshes:
+    IAnimatedMeshSceneNode* node;
     std::vector<std::string>::iterator itr;
     for (itr = geos.begin(); itr != geos.end(); ++itr)
     {
-        IAnimatedMesh* mesh;
-        mesh = smgr->getMesh((*itr).data());
+        // Load in obj from file:
+        SAnimatedMesh* mesh;    
+        mesh = (SAnimatedMesh*)smgr->getMesh((*itr).data());
 	    if (mesh)
-        	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
+        {   
+            // Get to te mesh buffer, create and append new ones:
+            std::cout << "Meshes:" << mesh->Meshes.size() << std::endl;
+            SMesh *geo_frame = 0;
+            SMeshBuffer *geo_buff = 0;
+            geo_frame = (SMesh*) mesh->Meshes[0];
+            std::cout << "Buffers in geo_frame: " << geo_frame->getMeshBufferCount()  << std::endl;
+            //geo_frame->getMeshBufferCount()
+            for (int b = 0; b < 1; b++)
+            {
+                geo_buff  = (SMeshBuffer*) geo_frame->MeshBuffers[2];            
+                //std::cout << "Vertices in geo_buffer: "<< geo_buff->getVertexCount() << std::endl;
+                for (int i = 0; i < 1; i++)
+                {
+                    SMeshBuffer  *new_buff = new SMeshBuffer();
+                    new_buff->Vertices.set_used(geo_buff->getVertexCount());
+                    // Set position per vertex:
+                    for (int j = 0; j < geo_buff->getVertexCount(); j++)
+                    {
+                        vector3df pos = geo_buff->getPosition(j);
+                        pos = pos + (pos/10.0);
+                        std::cout << "Vertex: " << j << ", index: " << geo_buff->getIndices()[j] << ": " << pos.X << ", " << pos.Y << ", " << pos.Z << ", " << std::endl;
+                        S3DVertex& v  = new_buff->Vertices[j];
+                        v.Pos.set(pos);
+                    }
+                    //geo_frame->MeshBuffers.push_back(new_buff);
+                   
+                }
+            }
+            mesh->recalculateBoundingBox();
+            mesh->setDirty();
+            std::cout << "Bufers count:" << mesh->getMeshBufferCount() << std::endl;
+        	node = smgr->addAnimatedMeshSceneNode(mesh);
+        }
     }
 
 
     // Add camera:
-    ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya();
-    camera->setPosition(vector3df(json_camera.tx[0], json_camera.ty[0], json_camera.tz[0]));
-    camera->setRotation(vector3df(json_camera.rx[0], json_camera.ry[0], json_camera.rz[0]));
-    float fov = 2 * atan( (24.0/2.0) / 40 );
+    ICameraSceneNode *camera = smgr->addCameraSceneNodeMaya(node, -100, 200, 100, -1, true);
+    //ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS(0, 1, 1);
+    //ICameraSceneNode *camera = smgr->addCameraSceneNode(0, vector3df(0,0,0), vector3df(0,0,0), true);
+    camera->bindTargetAndRotation(true);
+    //camera->setNearValue(0.1f);
+    //camera->setNearValue(1000.0f);
+    //camera->setInputReceiverEnabled(0);
+    //camera->setPosition(core::vector3df(json_camera.tz[0], json_camera.ty[0], json_camera.tx[0]));
+     // camera->setPosition(core::vector3df(,2,123));
+    //camera->setRotation(core::vector3df(json_camera.rx[0], json_camera.ry[0], json_camera.rz[0]));
+    float fov = 2 * atan( (24.0/2.0) / 40.0 );
     camera->setFOV(fov);
+    //smgr->setActiveCamera(camera);
+
+
+    std::cout << "Translate:";
+    std::cout << json_camera.tx[0] << ", " << json_camera.ty[0] << ", " << json_camera.tz[0] << std::endl;
+    std::cout << "Rotation:";
+    std::cout << json_camera.rx[0] << ", " << json_camera.ry[0] << ", " << json_camera.rz[0] << std::endl;
 
     // Lights:     
-	smgr->addLightSceneNode(camera, core::vector3df(300,100,-50), video::SColorf(1.0f,1.0f,1.0f),300);
+	smgr->addLightSceneNode(camera, core::vector3df(300,100,-300), video::SColorf(1.0f,1.0f,1.0f),300);
 	smgr->setAmbientLight(video::SColorf(0.3f,0.3f,0.3f));
 
-
+  
     // Capition:
     //const char *title = sprintf("Animation preview. Maximum primitives: %i", max_prims);
     device->setWindowCaption(L"Animation preview.");
     // On screen massage:
-    guienv->addStaticText(L"This is hardware render_preview test", rect<s32>(10,10,260,22), true);
+    guienv->addStaticText(L"This is hardware render_preview test", rect<s32>(10,10,260,44), true);
+
+    IrrlichtDelayFPS delayFPS;
+    delayFPS.maxFPS = 25; //set max FPS
+    delayFPS.device = device; //set device
+
     // Main rendering loop:
     while(device->run())
 	{
@@ -224,6 +299,7 @@ int main(int argc, char *argv[])
 		smgr->drawAll();
 		guienv->drawAll();
 		driver->endScene();
+        delayFPS.delay();
 	}
 
     // Ending up:
