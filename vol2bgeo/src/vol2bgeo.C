43209a073db7acc0b9fff55bf5e80296febae378
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2018-08-28 20:47:08 +0200

    H16.5 comp. fix and two stupid memory leaks...

--- vol2bgeo/src/vol2bgeo.C
+++ vol2bgeo/src/vol2bgeo.C
@@ -14,13 +14,13 @@ using namespace std;
 // Header structure of *.vol file
 struct VolumeHeader
 {
-	char magic[4];
-	int version;
-	char texName[256];
-	bool wrap;
-	int volSize;
-	int numChannels;
-	int bytesPerChannel;
+    char magic[4];
+    int version;
+    char texName[256];
+    bool wrap;
+    int volSize;
+    int numChannels;
+    int bytesPerChannel;
 };
 
 
@@ -37,36 +37,36 @@ int createBgeo(GU_Detail *gdp, unsigned char *voxels, int numChannels, int size)
 {
     //GU_PrimVolume * volumes[header->numChannels];
 
-	// Create bgeo and helpers for three volumes:
-	// I don't know why, but I can't do all fields in one loop.
-	// Program hangs if I write to more then one handle (?)
+    // Create bgeo and helpers for three volumes:
+    // I don't know why, but I can't do all fields in one loop.
+    // Program hangs if I write to more then one handle (?)
     int x, y, z, v;
     UT_Matrix3 xform;
     xform.identity();
     xform.scale(.5, .5, .5);
 
-	for (v=0; v<numChannels; v++)
-	{
-		GU_PrimVolume *volume = (GU_PrimVolume *)GU_PrimVolume::build(gdp);
-        volume->getVertex().getPt()->getPos() = UT_Vector3(0.5, 0.5, 0.5);
+    for (v=0; v<numChannels; v++)
+    {
+        GU_PrimVolume *volume = (GU_PrimVolume *)GU_PrimVolume::build(gdp);
+        volume->getDetail().setPos3(volume->getPointOffset(0), UT_Vector3(0.5, 0.5, 0.5));
         volume->setTransform(xform);
-		volume->setCompressionTolerance(.1);
-		UT_VoxelArrayWriteHandleF   handle = volume->getVoxelWriteHandle();
-		handle->size(size, size, size);
+        volume->setCompressionTolerance(.1);
+        UT_VoxelArrayWriteHandleF   handle = volume->getVoxelWriteHandle();
+        handle->size(size, size, size);
 
-	// and iterate over interleased fields:
-		{
-		for (z=0; z < size; z++)
-			for (y=0; y < size; y++)
-				for (x=0; x < size; x++) 
-				{
-					int value = voxels[v+(x+y*size+z*size*size)*3];
-					handle->setValue(z, y, x, (float)value * (1.0/256));
-				}	
-		}
-     	volume->recompress();
-	}
-	return 1;
+    // and iterate over interleased fields:
+        {
+        for (z=0; z < size; z++)
+            for (y=0; y < size; y++)
+                for (x=0; x < size; x++) 
+                {
+                    int value = voxels[v+(x+y*size+z*size*size)*3];
+                    handle->setValue(z, y, x, (float)value * (1.0/256));
+                }   
+        }
+        volume->recompress();
+    }
+    return 1;
 } 
 
 
@@ -74,72 +74,72 @@ int createBgeo(GU_Detail *gdp, unsigned char *voxels, int numChannels, int size)
 
 int main(int argc, char *argv[]) {
 
-	// Any arguments?
-	if ( argc < 3 ) 
-	{
-		cout << "\nUsage: vol2bgeo infile.vol outfile.bgeo\n" << endl; 
-		return 1; 
-	}
-	UT_String inputname, outputname;
+    // Any arguments?
+    if ( argc < 3 ) 
+    {
+        cout << "\nUsage: vol2bgeo infile.vol outfile.bgeo\n" << endl; 
+        return 1; 
+    }
+    UT_String inputname, outputname;
 
     inputname.harden(argv[1]);
     outputname.harden(argv[2]);
 
-	// Init some vars:                
-	unsigned char *voxels = NULL;
-	int success;
-	char buf[4096];
+    // Init some vars:                
+    unsigned char *voxels = NULL;
+    int success;
+    char buf[4096];
    
-	
-	/// Open input *.vol file:
-	FILE * fin = fopen(inputname.buffer(), "rb");
-	if (fin == NULL) 
-	{
-	    cerr << "Can't open file file " << inputname << endl;
-	    return 1;
-	}
-	
-	success = fread(buf, 4096, 1, fin);
-	
-	if (!success)
-	{
-	    cerr << "Couldn't load a header from "<< inputname << endl;
-	    return 1;
-	}
-	
-	VolumeHeader *header = (VolumeHeader *)buf;
-	int volBytes = header->volSize*header->volSize*header->volSize*header->numChannels;
-	voxels = new unsigned char[volBytes];
-	int size   = header->volSize;
-	success = fread(voxels, volBytes, 1, fin);		
-	fclose(fin);
-	
-	if (!success)
-	{
-	    cerr << "Can't load voxels from" << inputname << endl;
-	    return 1;
-	}
-	
-	/// Print some statistics:
-	cout << "Input: " << inputname << endl;
-	cout << "Output: "<< outputname << endl;
-	cout << "Volume Size (cubic): " << size << endl;
-	cout << "Num of Channels: " << header->numChannels << endl;
-	cout << "Bytes per Channel: " << header->bytesPerChannel << endl;
-	cout << "Wrap texture: " << header->wrap << endl;
-	
-	 /// Create Volume primitives from raw voxels:
-	if (!strcmp(outputname.fileExtension(), ".bgeo"))
-	{
+    
+    /// Open input *.vol file:
+    FILE * fin = fopen(inputname.buffer(), "rb");
+    if (fin == NULL) 
+    {
+        cerr << "Can't open file file " << inputname << endl;
+        return 1;
+    }
+    
+    success = fread(buf, 4096, 1, fin);
+    
+    if (!success)
+    {
+        cerr << "Couldn't load a header from "<< inputname << endl;
+        return 1;
+    }
+    
+    VolumeHeader *header = (VolumeHeader *)buf;
+    int volBytes = header->volSize*header->volSize*header->volSize*header->numChannels;
+    voxels = new unsigned char[volBytes];
+    int size   = header->volSize;
+    success = fread(voxels, volBytes, 1, fin);      
+    fclose(fin);
+    
+    if (!success)
+    {
+        cerr << "Can't load voxels from" << inputname << endl;
+        return 1;
+    }
+    
+    /// Print some statistics:
+    cout << "Input: " << inputname << endl;
+    cout << "Output: "<< outputname << endl;
+    cout << "Volume Size (cubic): " << size << endl;
+    cout << "Num of Channels: " << header->numChannels << endl;
+    cout << "Bytes per Channel: " << header->bytesPerChannel << endl;
+    cout << "Wrap texture: " << header->wrap << endl;
+    
+     /// Create Volume primitives from raw voxels:
+    if (!strcmp(outputname.fileExtension(), ".bgeo"))
+    {
         GU_Detail *gdp = new GU_Detail();
         success = createBgeo(gdp, voxels, header->numChannels, size);
-        delete voxels;
-	    if (success)
-	    {
-	        gdp->save(outputname.buffer(), 1, 0);
-	        delete gdp;
-	        return 0;
-	    }	    
+        delete[] voxels;
+        if (success)
+        {
+            gdp->save(outputname.buffer(), nullptr);
+            delete gdp;
+            return 0;
+        }       
     }
     /// Create i3d texture out of raw voxels:
     if (!strcmp(outputname.fileExtension(), ".i3d"))
@@ -162,11 +162,11 @@ int main(int argc, char *argv[]) {
         if (success)
             cout << "I3d saved."<< endl;
         
-        delete voxels;
-        delete fvoxels;
+        delete[] voxels;
+        delete[] fvoxels;
         return 0;
     }
-	
-	return 1;
+    
+    return 1;
 
 }
